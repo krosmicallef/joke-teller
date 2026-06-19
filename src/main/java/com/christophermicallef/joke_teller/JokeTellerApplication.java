20861b9519f1414f9038b070e4af0ec2a8f139bc
Chris Micallef <krosmicallef@gmail.com>
2026-06-19 15:03:03 +0200

    first commit

---
+++ src/main/java/com/christophermicallef/joke_teller/JokeTellerApplication.java
@@ -0,0 +1,13 @@
+package com.christophermicallef.joke_teller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeTellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christophermicallef/joke_teller/JokeTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christophermicallef.joke_teller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeTellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
